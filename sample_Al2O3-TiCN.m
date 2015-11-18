0f49dc004a3346d26dc2ae42b9bdf5c76ea41d10
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2015-11-18 15:33:55 +0800

    lowered stress values

--- sample_Al2O3-TiCN.m
+++ sample_Al2O3-TiCN.m
@@ -3,7 +3,7 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [-6000. -3500. 0.];
+sample.layer(i).sigma_z_model = [-6000. -3500. -100.];
 i = 2;
 sample.layer(i).material = 'TiCN';
 sample.layer(i).diffracting = false;
@@ -14,7 +14,7 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [400. 350. 300.];
+sample.layer(i).sigma_z_model = [-800. -600. -550.];
 i = 4;
 sample.layer(i).material = 'TiCN';
 sample.layer(i).diffracting = false;
@@ -25,17 +25,17 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [150. 120. 100.];
+sample.layer(i).sigma_z_model = [-250. -200. -220.];
 
 sample.sigma_tau = [
-    0.05   -5880   85
-    0.25   -5577   92
-    0.45   -5098   58
-    0.65   -4836   30
-    0.85   -4500   22
-    1.30   -4165   23
-    1.80   -3835   22
-    2.30   -3555   25
-    2.80   -3293   20
-    3.4    -3036   18
+    0.05  -5922  85
+    0.25  -5563  92
+    0.45  -5075  58
+    0.65  -4821  30
+    0.85  -4497  22
+    1.30  -4179  23
+    1.80  -3864  22
+    2.30  -3602  25
+    2.80  -3418  20
+    3.4   -3195  18
 ];
