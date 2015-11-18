f9d34cab784c0507c3960745d2912e1b1778f6f3
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2015-11-18 11:09:08 +0800

    added 'sampleTemplate.m' description

--- Contents.m
+++ Contents.m
@@ -1,20 +1,18 @@
-% thinstress - The residual stress evaluation in thin films.
-% Version 1.0.0  4-October-2015
+% Thinstress: Residual stress depth profiling in thin films.
+% Version 1.0.0  4-November-2015
 %
-% config.m          - Configuration variables for the Tikhonov regularization.
-% numFredholm.m     - Numerical discretization of the Fredholm integral
-%                     equation.
-% plotResults.m     - Plot calculated residual stress profiles.
-% postProcessing.m  - Perfom a post-processing of the results.
-% preProcessing.m   - Perform a pre-processing of the initial data.
-% printStructure.m  - Print layers' structure in the multilayered system.
-% regTikhonov.m     - Find the regularized solution to the ill-posed inverse
-%                     problem.
-% calcStress.m      - Calculate residual stresses in diffracting material.
-%
-% Directory 'models'
-%   sampleLayered.m  - User defined structure of the multilayered sample.
-%   sampleSolid.m    - User defined structure of the solid sample.
+% config.m         - Configuration variables for the Tikhonov regularization.
+% numFredholm.m    - Numerical discretization of the Fredholm integral
+%                    equation.
+% plotResults.m    - Plot calculated residual stress profiles.
+% postProcessing.m - Perfom a post-processing of the results.
+% preProcessing.m  - Perform a pre-processing of the initial data.
+% printStructure.m - Print layers' structure in the multilayered system.
+% regTikhonov.m    - Find the regularized solution to the ill-posed inverse
+%                    problem.
+% calcStress.m     - Calculate residual stresses in diffracting material.
+% sampleTemplate.m - Template of the 'sample' structure array which describes a
+%                    multilayered coating system (MLS).
  
 % Authors
 %   Konstantin Tolstikhin <k.tolstikhin(at)gmail.com>
