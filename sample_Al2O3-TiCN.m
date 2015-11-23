95f75328c31063dba35772d5f5e3d82ffcd91343
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2015-11-23 10:28:18 +0800

    lowered perturbation of the initial data

--- sample_Al2O3-TiCN.m
+++ sample_Al2O3-TiCN.m
@@ -3,7 +3,7 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [-6000. -3500. -100.];
+sample.layer(i).sigma_z_model = [-6500. -3800. -100.];
 i = 2;
 sample.layer(i).material = 'TiCN';
 sample.layer(i).diffracting = false;
@@ -14,7 +14,7 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [-800. -600. -550.];
+sample.layer(i).sigma_z_model = [-800. -600. -650.];
 i = 4;
 sample.layer(i).material = 'TiCN';
 sample.layer(i).diffracting = false;
@@ -25,17 +25,19 @@ sample.layer(i).material = 'Al2O3';
 sample.layer(i).diffracting = true;
 sample.layer(i).thickness = 2.3;
 sample.layer(i).mue = 192.17;
-sample.layer(i).sigma_z_model = [-250. -200. -220.];
+sample.layer(i).sigma_z_model = [-250. -200. -240.];
 
 sample.sigma_tau = [
-    0.05  -5922  85
-    0.25  -5563  92
-    0.45  -5075  58
-    0.65  -4821  30
-    0.85  -4497  22
-    1.30  -4179  23
-    1.80  -3864  22
-    2.30  -3602  25
-    2.80  -3418  20
-    3.4   -3195  18
+    0.05  -6409  25
+    0.15  -6224  22
+    0.25  -6003  20
+    0.30  -5896  12
+    0.45  -5562  18
+    0.65  -5199  10
+    0.85  -4918  11
+    1.30  -4491  10
+    1.80  -4186  9
+    2.30  -3933  8
+    2.80  -3694  7
+    3.4   -3459  8
 ];
